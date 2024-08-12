ad4232f46681412bc74b8f5446803421ffb613f8
Lala1509 <00001089259086sp@al.educacao.sp.gov.br>
2024-08-12 08:44:14 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,29 @@
+ let palavra
+  function setup() {
+    createCanvas(400, 400);
+    palavra = palavraAleatoria();
+  }
+
+ function palavraAleatoria() {
+   let palavras = ["caminhante", "caminho", "caminha"];
+   return random(palavras);
+ }
+function inicializarCores() {
+  background("white");
+  fill("black");
+  textSize (64);
+  textAlign(CENTER,CENTER);
+}
+
+function  palavraParcial (minimo,maximo) {
+  let quantidade= map(mouseX,minimo,maximo,1, palavra.length); let parcial = palavra.substring(0,quantidade);
+  return parcial;
+}
+
+function draw() {
+   inicializarCores();
+  
+  let texto = palavraParcial(0, width);
+  
+  text(texto,200,200);
+}
